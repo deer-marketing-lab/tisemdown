3251ea6d856441d0129bca2220ada31261e342d3
Lachlan Deer <lachlan.deer@gmail.com>
2021-06-14 20:16:11 +0200

    next attempt at a hexsticker

--- images/hex_creator.R
+++ images/hex_creator.R
@@ -1,16 +1,17 @@
 library(hexSticker)
 
 # https://pixabay.com/illustrations/graduation-hats-throw-graduation-4906359/
+# can we change to this? https://www.google.com/url?sa=i&url=https%3A%2F%2Fappadvice.com%2Fgame%2Fapp%2Fosiris-tilburg-university%2F1418538742&psig=AOvVaw0BZm0aV9mPxbZf950Qyg36&ust=1623761906652000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPjs0NKWl_ECFQAAAAAdAAAAABAE
 cap <- "images/happy_cap.png"
 
 sticker(cap,
   package = "tisemdown",
   p_family = "Aller_Rg",
-  p_size = 7, p_color = "#5B5040", # p_x = 4, p_y = 1.4,
-  s_x = 1, s_y = .8, s_width = 0.75, s_height = 0.75,
-  h_fill = "#FAECC7", h_color = "#5B5040",
-  filename = "thesisdown_hex.png",
-  url = "thesisdown.rbind.io"
+  s_x=1, s_y=1.15, s_width=.8, p_y = 0.45, p_size = 14, h_fill = "#FFCF68",
+        h_color = "#003366", p_color = "#003366", h_size = 1.2,
+        u_size = 3, u_color = "#003366",
+  filename = "tisemdown_hex.png",
+  url = "deer-marketing-lab.github.io/tisemdown"
 )
 
 # Final image was revised in Adobe Illustrator
